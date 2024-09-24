834855d03a06d8dcb05fa2cf37903b44dbc131f8
WataminC <WataminC@outlook.com>
2024-09-24 11:14:42 +0800

    Implement stream reassembler roughly butmay cause eof error

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -14,13 +14,102 @@ using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
 
+// void StreamReassembler::push_stream(const string &data) {
+//     willing += data.size();
+//     _output.write(data);
+//     size +=
+
+//     while(_) {
+
+//     }
+// }
+
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    string copy_data = data;
+    size_t copy_index = index;
+
+    if (index < willing && index + copy_data.size() - 1 >=index) {
+        // copy_data = copy_data.substr(willing - index);
+        copy_data.erase(0, willing - index);
+        copy_index = willing;
+    }
+
+    if (_output.buffer_size() + reassembler.size() + copy_data.size() > _capacity) {   // check does the data fix the capacity
+        return ;
+    }
+
+    // size += data.size();
+    reassembler[copy_index] = std::move(copy_data);
+
+    end_flag = eof;
+
+    // Writing assembled block to the stream
+    // for (;;) {
+    //     // auto it = reassembler.find(willing);
+    //     // if (it == reassembler.end()) {
+    //     //     break;
+    //     // }
+    //     // std::string temp_str = std::move(it->second);
+    //     // reassembler.erase(it);
+
+    //     // willing += temp_str.size();
+    //     // _output.write(temp_str);
+    //     size_t temp_will = willing;
+    //     for (auto element = reassembler.begin(); element != reassembler.end(); element++) {
+    //         if (element->first > willing || element->first + element->second.size() - 1 < willing) {
+    //             continue;
+    //         }
+
+    //         std::string temp_str = std::move(element->second.substr(willing - element->first));
+    //         reassembler.erase(element);
+
+    //         willing += temp_str.size();
+    //         _output.write(temp_str);
+    //     }
+
+    //     if (temp_will == willing) {
+    //         break;
+    //     }
+    // }
+
+    for (;;) {
+        size_t temp_will = willing;
+        
+        for (auto element = reassembler.begin(); element != reassembler.end(); /* no increment here */) {
+            if (element->first > willing || element->first + element->second.size() - 1 < willing) {
+                ++element;  // Increment if not erasing
+                continue;
+            }
+
+            // Get the substring from the element
+            std::string temp_str = element->second.substr(willing - element->first);
+            
+            // Erase the element and update the iterator
+            element = reassembler.erase(element);
+
+            // Update the willing value and write to output
+            willing += temp_str.size();
+            _output.write(temp_str);
+        }
+
+        if (temp_will == willing) {
+            break;
+        }
+    }
+
+
+    if (end_flag && reassembler.size() == 0) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return reassembler.size();
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+    return reassembler.size() == 0;
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 #include <string>
+#include <unordered_map>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +15,10 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t willing{};    // the next index that can be snitched to the stream 
+    std::unordered_map<size_t, std::string> reassembler{};
+    bool end_flag{};
+    // size_t size{};
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
@@ -30,6 +35,8 @@ class StreamReassembler {
     //! \param index indicates the index (place in sequence) of the first byte in `data`
     //! \param eof the last byte of `data` will be the last byte in the entire stream
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
+
+    // void push_stream(const std::string &data);
 
     //! \name Access the reassembled byte stream
     //!@{
