d3a1812d57c16dbced579eb27766cb8b7ed0ef6d
WataminC <1286982501@qq.com>
2024-09-25 11:56:21 +0800

    Lab2: Finish wrapping_number part but implementing ugly

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <cmath>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +15,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    // DUMMY_CODE(n, isn);
+    uint64_t max_number = 1ULL << 32;
+    n = n % max_number;
+    return isn + n;
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +32,26 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    // DUMMY_CODE(n, isn, checkpoint);
+    uint64_t max_number = 1ULL << 32;
+    uint64_t times = checkpoint / max_number;
+
+    uint64_t A = static_cast<uint64_t>(WrappingInt32(n - isn).raw_value());
+    uint64_t B = checkpoint % max_number;
+
+    // uint64_t ans1 = times * max_number + (n - isn);
+    // if (((A <= B && A >= B - max_number/2) || (B == 0 && A <= max_number / 2)) || (A > B && A <= B + max_number / 2)) {
+    if ((A <= B && (B <= max_number / 2 || A >= B - max_number/2)) || (A > B && (B >= max_number / 2|| A <= B + max_number / 2))) {
+        return times * max_number + A;
+    }
+    else if (A <= B) {
+        return (times + 1) * max_number + A;
+    }
+    else {
+        if (times < 1)
+            return times * max_number + A;
+        return (times - 1) * max_number + A;
+    }
+
+    return -1;
 }
